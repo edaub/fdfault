0b42a726275285ec9792fdf084bfdf8b61a4eed3
Eric Daub <egdaub@memphis.edu>
2015-04-13 11:32:51 -0500

    cleaned up master branch, changed input files, ongoing development on devel branch

--- problems/input_example.py
+++ problems/input_example.py
@@ -1,10 +1,9 @@
 import fdfault
 import numpy as np
-import matplotlib.pyplot as plt
 
 # create problem
 
-p = fdfault.problem('testsurf')
+p = fdfault.problem('testprob')
 
 # set rk and fd order
 
@@ -34,16 +33,23 @@ p.set_bounds((1,0,0),['none', 'absorbing', 'absorbing', 'absorbing'])
 
 # set block surface
 
+# create arrays for curve
+
 y = np.linspace(0., 32., 1601)
 x = 12.*np.ones(1601)+0.5*np.sin(np.pi*y/16.)
+
+# set normals (convention is that normals always point in "positive" direction
 m = np.pi*0.5/16.*np.cos(np.pi*y/16.)
 ny = -m/np.sqrt(1.+m**2)
 nx = 1./np.sqrt(1.+m**2)
 
+# since this is a 2D problem, boundary is of curve type
+# for 3D problem, use surface class
+
 surf = fdfault.curve(1601, 'x', x, y, nx, ny)
 
-#p.set_block_surf((0,0,0), 1, surf)
-#p.set_block_surf((1,0,0), 0, surf)
+p.set_block_surf((0,0,0), 1, surf)
+p.set_block_surf((1,0,0), 0, surf)
 
 # set initial fields
 
@@ -55,7 +61,7 @@ p.set_iftype(0,'slipweak')
 
 # set interface surface
 
-#p.set_iface_surf(0,surf)
+p.set_iface_surf(0,surf)
 
 # set slip weakening parameters
 
@@ -67,9 +73,8 @@ p.add_load(fdfault.load('boxcar',0., 16., 1.5, 0., 0., 0., 11.6, 0.))
 
 # add output unit
 
+p.add_output(fdfault.output('vxbody','vx',0, 1600, 50, 0, 1200, 2, 0, 1600, 2, 0, 0, 1))
 p.add_output(fdfault.output('vybody','vy',0, 1600, 50, 0, 1200, 2, 0, 1600, 2, 0, 0, 1))
 p.add_output(fdfault.output('vfault','V',0, 1600, 50, 601, 601, 1, 0, 1600, 2, 0, 0, 1))
 
 p.write_input()
-
-plt.show()
