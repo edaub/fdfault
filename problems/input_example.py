580e417f6757fc33438c76a7e36880fd75a203fe
Eric Daub <egdaub@memphis.edu>
2015-04-12 22:06:13 -0500

    minor changes in example problem setup

--- problems/input_example.py
+++ problems/input_example.py
@@ -1,10 +1,9 @@
 import fdfault
 import numpy as np
-import matplotlib.pyplot as plt
 
 # create problem
 
-p = fdfault.problem('testsurf')
+p = fdfault.problem('testprob')
 
 # set rk and fd order
 
@@ -13,7 +12,7 @@ p.set_sbporder(4)
 
 # set time step info
 
-p.set_nt(1601)
+p.set_nt(101)
 p.set_cfl(0.3)
 p.set_ninfo(50)
 
@@ -34,16 +33,16 @@ p.set_bounds((1,0,0),['none', 'absorbing', 'absorbing', 'absorbing'])
 
 # set block surface
 
-y = np.linspace(0., 32., 1601)
-x = 12.*np.ones(1601)+0.5*np.sin(np.pi*y/16.)
-m = np.pi*0.5/16.*np.cos(np.pi*y/16.)
-ny = -m/np.sqrt(1.+m**2)
-nx = 1./np.sqrt(1.+m**2)
-
-surf = fdfault.curve(1601, 'x', x, y, nx, ny)
-
-#p.set_block_surf((0,0,0), 1, surf)
-#p.set_block_surf((1,0,0), 0, surf)
+##y = np.linspace(0., 32., 1601)
+##x = 12.*np.ones(1601)+0.5*np.sin(np.pi*y/32.)
+##m = np.pi*0.5/32.*np.cos(np.pi*y/32.)
+##ny = -m/np.sqrt(1.+m**2)
+##nx = 1./np.sqrt(1.+m**2)
+##
+##surf = fdfault.curve(1601, 'x', x, y, nx, ny)
+##
+##p.set_block_surf((0,0,0), 1, surf)
+##p.set_block_surf((1,0,0), 0, surf)
 
 # set initial fields
 
@@ -64,12 +63,13 @@ p.set_params(0.4, 0.677, 0.525)
 # add load perturbations
 
 p.add_load(fdfault.load('boxcar',0., 16., 1.5, 0., 0., 0., 11.6, 0.))
+p.add_load(fdfault.load('boxcar',0., 0., 4., 0., 0., -10000., 0., 0.))
+p.add_load(fdfault.load('boxcar',0., 32., 4., 0., 0., -10000., 0., 0.))
 
 # add output unit
 
-p.add_output(fdfault.output('vybody','vy',0, 1600, 50, 0, 1200, 2, 0, 1600, 2, 0, 0, 1))
-p.add_output(fdfault.output('vfault','V',0, 1600, 50, 601, 601, 1, 0, 1600, 2, 0, 0, 1))
+p.add_output(fdfault.output('vxbody','vx',0, 100, 50, 0, 1200, 2, 0, 1600, 2, 0, 0, 1))
+p.add_output(fdfault.output('vybody','vy',0, 100, 50, 0, 1200, 2, 0, 1600, 2, 0, 0, 1))
+p.add_output(fdfault.output('vfault','V',0, 100, 50, 601, 601, 1, 0, 1600, 2, 0, 0, 1))
 
 p.write_input()
-
-plt.show()
