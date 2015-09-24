6d6dbce46829fc28a8c1de3eef2fdd5c463261d0
Eric Daub <egdaub@memphis.edu>
2015-09-24 16:35:34 -0500

    fixed makefile and corrected example 2d python problem

--- problems/input_example.py
+++ problems/input_example.py
@@ -12,7 +12,7 @@ p.set_sbporder(4)
 
 # set time step info
 
-p.set_nt(101)
+p.set_nt(1601)
 p.set_cfl(0.3)
 p.set_ninfo(50)
 
@@ -33,16 +33,13 @@ p.set_bounds((1,0,0),['none', 'absorbing', 'absorbing', 'absorbing'])
 
 # set block surface
 
-##y = np.linspace(0., 32., 1601)
-##x = 12.*np.ones(1601)+0.5*np.sin(np.pi*y/32.)
-##m = np.pi*0.5/32.*np.cos(np.pi*y/32.)
-##ny = -m/np.sqrt(1.+m**2)
-##nx = 1./np.sqrt(1.+m**2)
-##
-##surf = fdfault.curve(1601, 'x', x, y, nx, ny)
-##
-##p.set_block_surf((0,0,0), 1, surf)
-##p.set_block_surf((1,0,0), 0, surf)
+y = np.linspace(0., 32., 1601)
+x = 12.*np.ones(1601)+0.5*np.sin(np.pi*y/32.)
+
+surf = fdfault.curve(1601, 'x', x, y)
+
+p.set_block_surf((0,0,0), 1, surf)
+p.set_block_surf((1,0,0), 0, surf)
 
 # set initial fields
 
@@ -52,24 +49,20 @@ p.set_stress((-120., 70., 0., -100., 0., 0.))
 
 p.set_iftype(0,'slipweak')
 
-# set interface surface
-
-p.set_iface_surf(0,surf)
-
 # set slip weakening parameters
 
-p.set_params(0.4, 0.677, 0.525)
+p.add_pert(fdfault.swparam('constant', dc = 0.4, mus = 0.676, mud = 0.525),0)
+p.add_pert(fdfault.swparam('boxcar', x0 = 2., dx = 2., mus = 10000.),0)
+p.add_pert(fdfault.swparam('boxcar', x0 = 30., dx = 2., mus = 10000.),0)
 
 # add load perturbations
 
 p.add_load(fdfault.load('boxcar',0., 16., 1.5, 0., 0., 0., 11.6, 0.))
-p.add_load(fdfault.load('boxcar',0., 0., 4., 0., 0., -10000., 0., 0.))
-p.add_load(fdfault.load('boxcar',0., 32., 4., 0., 0., -10000., 0., 0.))
 
 # add output unit
 
-p.add_output(fdfault.output('vxbody','vx',0, 100, 50, 0, 1200, 2, 0, 1600, 2, 0, 0, 1))
-p.add_output(fdfault.output('vybody','vy',0, 100, 50, 0, 1200, 2, 0, 1600, 2, 0, 0, 1))
-p.add_output(fdfault.output('vfault','V',0, 100, 50, 601, 601, 1, 0, 1600, 2, 0, 0, 1))
+p.add_output(fdfault.output('vxbody','vx',0, 1600, 50, 0, 1200, 2, 0, 1600, 2, 0, 0, 1))
+p.add_output(fdfault.output('vybody','vy',0, 1600, 50, 0, 1200, 2, 0, 1600, 2, 0, 0, 1))
+p.add_output(fdfault.output('vfault','V',0, 1600, 10, 601, 601, 1, 0, 1600, 2, 0, 0, 1))
 
 p.write_input()
