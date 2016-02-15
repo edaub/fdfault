c69df0f78011498cf91e8b46a10d29eadf07daab
Eric Daub <egdaub@memphis.edu>
2016-02-15 17:36:06 -0600

    fixed minor bug in outputunit interface locator

--- src/outputunit.cpp
+++ src/outputunit.cpp
@@ -74,21 +74,21 @@ outputunit::outputunit(const string probname, const string datadir, const int nt
             direction = d.interfaces[i]->direction;
             if (direction == 0) {
                 if ((xm_in[0] == d.interfaces[i]->xm[0] || xm_in[0] == d.interfaces[i]->xm[0]-1) && (xm_in[1] >= d.interfaces[i]->xm[1]) &&
-                    (xm_in[1] < d.interfaces[i]->xp[1]) && (xm_in[2] >= d.interfaces[i]->xm[2]) &&
+                    (xm_in[1] <= d.interfaces[i]->xp[1]) && (xm_in[2] >= d.interfaces[i]->xm[2]) &&
                     (xm_in[2] <= d.interfaces[i]->xp[2])) {
                     location = i;
                     break;
                 }
             } else if (direction == 1) {
                 if ((xm_in[1] == d.interfaces[i]->xm[1] || xm_in[1] == d.interfaces[i]->xm[1]-1) && (xm_in[0] >= d.interfaces[i]->xm[0]) &&
-                    (xm_in[0] < d.interfaces[i]->xp[0]) && (xm_in[2] >= d.interfaces[i]->xm[2]) &&
+                    (xm_in[0] <= d.interfaces[i]->xp[0]) && (xm_in[2] >= d.interfaces[i]->xm[2]) &&
                     (xm_in[2] <= d.interfaces[i]->xp[2])) {
                     location = i;
                     break;
                 }
             } else {
                 if ((xm_in[2] == d.interfaces[i]->xm[2] || xm_in[2] == d.interfaces[i]->xm[2]-1) && (xm_in[0] >= d.interfaces[i]->xm[0]) &&
-                    (xm_in[0] < d.interfaces[i]->xp[0]) && (xm_in[1] >= d.interfaces[i]->xm[1]) &&
+                    (xm_in[0] <= d.interfaces[i]->xp[0]) && (xm_in[1] >= d.interfaces[i]->xm[1]) &&
                     (xm_in[1] <= d.interfaces[i]->xp[1])) {
                     location = i;
                     break;
