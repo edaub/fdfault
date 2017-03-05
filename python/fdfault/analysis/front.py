b3e8918a61228ef919498b00fc60ec7bbbe74fba
Eric Daub <egdaub@memphis.edu>
2017-03-05 15:05:52 -0600

    added docstrings for analysis classes in python module

--- python/fdfault/analysis/front.py
+++ python/fdfault/analysis/front.py
@@ -1,10 +1,37 @@
+"""
+``analysis.front`` is a class used for holding rupture time data for analysis with Python. The class
+contains information on the problem, the interface that was output, number of grid points, and
+byte-ordering of the output data. Once the class instance is initialized and the rupture time data
+is loaded, the class is designed to be used as a basic data structure in analysis routines and for
+plotting and visualizing data.
+"""
+
 import numpy as np
 from os import getcwd
 from os.path import join
 from sys import path
             
 class front(object):
-    "class for rupture front objects"
+    """
+    Class for rupture front objects
+
+    Object describing a rupture front, with the following attributes:
+
+    :ivar problem: Name of problem
+    :type problem: str
+    :ivar iface: Interface that was output
+    :type name: int
+    :ivar datadir: Directory where simulation data is held, if ``None`` (default) this is the current directory
+    :type datadir: str
+    :ivar nx: Number of x grid points (or number of y grid points if the target interface has an x normal)
+    :type nx: int
+    :ivar ny: Number of y grid points (or number of z grid points if the target interface has an x or y normal)
+    :type ny: int
+    :ivar endian: Byte-ordering of simulation data (``'='`` for native, ``'>'`` for big endian, ``'<'`` for little endian)
+    :type endian: str
+    :ivar t: Numpy array holding rupture time data
+    :type t: ndarray
+    """
     def __init__(self, problem, iface, datadir = None):
         "initializes output object with simulation information"
         
@@ -24,7 +51,18 @@ class front(object):
         self.endian = self._temp.endian
 
     def load(self):
-        "load data from data file for rupture front"
+        """
+        Load data from data file for rupture front
+
+        Method loads the data from file into the ``t`` attribute. If you have an existing instance of
+        an front class whose simulation data has changed, ``load`` can be run more than once
+        and will refresh the contents of the simulation output.
+
+        Method takes no inputs and has no outputs. Class is modified by running this method
+        as the simulation data will be reloaded from file if it already exists.
+
+        :returns: None
+        """
 
         # check if simulation data has changed
 
@@ -49,6 +87,6 @@ class front(object):
         self.t = np.squeeze(np.fromfile(join(self.datadir, self.problem+'_front_'+str(self.iface)+'_t.dat'), self.endian+'f8').reshape(self.nx,self.ny))
 
     def __str__(self):
-        "returns string representation"
+        "Returns a string representation of the rupture front"
         return ('Problem '+self.problem+', Front from interface '+str(self.iface)+'\nnx = '+str(self.nx)
                 +'\nny = '+str(self.ny))
--- python/fdfault/analysis/output.py
+++ python/fdfault/analysis/output.py
@@ -1,12 +1,55 @@
+"""
+``analysis.output`` is a class used for holding simulation data for analysis with Python. The class
+contains information on the problem, field, number of grid and time points, and byte-ordering
+of the output data. Once the class instance is initialized and the simulation data is loaded,
+the class is designed to be used as a basic data structure in analysis routines and for plotting
+and visualizing data.
+
+The field data itself is loaded into the ``fielddata`` attribute, a numpy array with the shape
+``(nt, nx, ny, nz)`` holding the simulation data. The ``load`` routine also creates an alias to
+``fielddata`` using the field name itself. This allows for flexibility writing routines that can
+operate on the ``output`` class generically, while also preserving code legibility for routines that
+operate on ``output`` classes with a certain field associated with it. Thus, if the ``field`` attribute
+on the output unit ``vx_body`` is ``vx``, then the x particle velocities can be accessed with either
+``vx_body.vx`` or ``vx_body.fielddata``.
+"""
+
 import numpy as np
 from os import getcwd
 from os.path import join
 from sys import path
 
 class output(object):
-    "class for output objects"
+    """
+    Class representing an output object
+
+    Output objects contain the following attributes:
+
+    :ivar problem: Name of problem
+    :type problem: str
+    :ivar name: Name of output unit
+    :type name: str
+    :ivar datadir: Directory where simulation data is held, if ``None`` (default) this is the current directory
+    :type datadir: str
+    :ivar field: Field that was saved to file
+    :type field: str
+    :ivar nt: Number of time steps
+    :type nt: int
+    :ivar nx: Number of x grid points
+    :type nx: int
+    :ivar ny: Number of y grid points
+    :type ny: int
+    :ivar nz: Number of z grid points
+    :type nz: int
+    :ivar endian: Byte-ordering of simulation data (``'='`` for native, ``'>'`` for big endian, ``'<'`` for little endian)
+    :type endian: str
+    :ivar fielddata: Numpy array holding simulation data (also aliased using the field name itself)
+    :type fielddata: ndarray
+    """
     def __init__(self, problem, name, datadir = None):
-        "initializes output object with simulation information"
+        """
+        Initializes output object with simulation information
+        """
         
         self.name = name
         self.problem = problem
@@ -27,7 +70,19 @@ class output(object):
         self.endian = self._temp.endian
 
     def load(self):
-        "load data from data file for output item"
+        """
+        Load data from data file for output item
+
+        Method loads the data from file into the ``fielddata`` attribute, a Numpy array, and also
+        creates an alias with the ``field`` attribute itself. If you have an existing instance of
+        an output class whose simulation data has changed, ``load`` can be run more than once
+        and will refresh the contents of the simulation output.
+
+        Method takes no inputs and has no outputs. Class is modified by running this method
+        as the simulation data will be reloaded from file if it already exists.
+
+        :returns: None
+        """
 
         # check if simulation data has changed
 
@@ -63,6 +118,6 @@ class output(object):
         setattr(self, self.field, self.fielddata)
 
     def __str__(self):
-        "returns string representation"
+        "Feturns a string representation of the output unit"
         return ('Problem '+self.problem+', Output '+self.name+'\nfield = '+self.field+'\nnt = '+str(self.nt)+'\nnx = '+str(self.nx)
                 +'\nny = '+str(self.ny)+'\nnz = '+str(self.nz))
