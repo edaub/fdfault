a205e06227079f49f328de4573922dfbd6b0821e
Eric Daub <45598892+edaub@users.noreply.github.com>
2020-05-14 17:16:58 +0100

    fixed bug in friction.cpp to fix issue with accessing unallocated memory when interface is not in a process (#4)

--- src/friction.cpp
+++ src/friction.cpp
@@ -19,33 +19,33 @@ using namespace std;
 friction::friction(const char* filename, const int ndim_in, const int mode_in, const string material_in, const int niface,
                    block**** blocks, const fields& f, const cartesian& cart, const fd_type& fd) : interface(filename, ndim_in, mode_in, material_in, niface, blocks, f, cart, fd) {
     // constructor initializes interface and then allocates memory for slip velocity and slip
-    
+
     is_friction = true;
-    
+
     // allocate memory for slip and slip rate arrays
-    
+
     if (!no_data) {
-    
+
         ux = new double [(ndim-1)*n_loc[0]*n_loc[1]];
         dux = new double [(ndim-1)*n_loc[0]*n_loc[1]];
         vx = new double [(ndim-1)*n_loc[0]*n_loc[1]];
         sx = new double [(ndim-1)*n_loc[0]*n_loc[1]];
-    
+
         u = new double [n_loc[0]*n_loc[1]];
         du = new double [n_loc[0]*n_loc[1]];
         v = new double [n_loc[0]*n_loc[1]];
         s = new double [n_loc[0]*n_loc[1]];
         sn = new double [n_loc[0]*n_loc[1]];
-        
+
         // initialize slip, change in slip, and slip velocity
-        
+
         for (int i=0; i<(ndim-1)*n_loc[0]*n_loc[1]; i++) {
             ux[i] = 0.;
             dux[i] = 0.;
             vx[i] = 0.;
             sx[i] = 0.;
         }
-        
+
         for (int i=0; i<n_loc[0]*n_loc[1]; i++) {
             u[i] = 0.;
             du[i] = 0.;
@@ -53,11 +53,11 @@ friction::friction(const char* filename, const int ndim_in, const int mode_in, c
             s[i] = 0.;
             sn[i] = 0.;
         }
-        
+
     }
-    
+
     // read loads from input file
-    
+
     string* ltype;
     double* t0;
     double* x0;
@@ -67,11 +67,11 @@ friction::friction(const char* filename, const int ndim_in, const int mode_in, c
     double* sl1;
     double* sl2;
     double* sl3;
-    
+
     stringstream ss;
-    
+
     ss << niface;
-    
+
     string line, loadfile;
     ifstream paramfile(filename, ifstream::in);
     if (paramfile.is_open()) {
@@ -127,14 +127,14 @@ friction::friction(const char* filename, const int ndim_in, const int mode_in, c
         MPI_Abort(MPI_COMM_WORLD,-1);
     }
     paramfile.close();
-    
+
     // create 2d lengths and locations for boundary perturbations
-    
+
     if (!no_data) {
-        
+
         double x_2d[2], l_2d[2];
         int xm_2d[2], xm_loc2d[2];
-        
+
         switch (direction) {
             case 0:
                 x_2d[0] = x[1];
@@ -166,15 +166,15 @@ friction::friction(const char* filename, const int ndim_in, const int mode_in, c
                 xm_loc2d[0] = xm_loc[0];
                 xm_loc2d[1] = xm_loc[1];
         }
-        
+
         loads = new load* [nloads];
-        
+
         for (int i=0; i<nloads; i++) {
             loads[i] = new load(ltype[i], t0[i], x0[i], dx[i], y0[i] , dy[i], n, xm_2d, xm_loc2d, x_2d, l_2d, sl1[i], sl2[i], sl3[i]);
         }
-        
+
     }
-    
+
     delete[] ltype;
     delete[] t0;
     delete[] x0;
@@ -184,66 +184,70 @@ friction::friction(const char* filename, const int ndim_in, const int mode_in, c
     delete[] sl1;
     delete[] sl2;
     delete[] sl3;
-    
+
     // if needed, read load data from file
-    
+
     if (loadfile == "none") {
         load_file = false;
     } else {
         load_file = true;
-        
+
         // allocate memory for loads
-        
+
         if (!no_data) {
-            
+
             s1 = new double [n_loc[0]*n_loc[1]];
             s2 = new double [n_loc[0]*n_loc[1]];
             s3 = new double [n_loc[0]*n_loc[1]];
-            
+
         }
-        
+
         // read load for each potential side of interface (may be read twice if both sides in process)
-        
+
         read_load(loadfile, !data1);
         read_load(loadfile, !data2);
-        
+
         // if 2d problem, override unused traction components by setting to zero
-        
-        if (ndim == 2 && mode == 2) {
-            for (int i=0; i<n_loc[0]*n_loc[1]; i++) {
-                s3[i] = 0.;
-            }
-        } else if (ndim == 2 && mode == 3) {
-            for (int i=0; i<n_loc[0]*n_loc[1]; i++) {
-                s2[i] = 0.;
+
+        if (!no_data) {
+
+            if (ndim == 2 && mode == 2) {
+                for (int i=0; i<n_loc[0]*n_loc[1]; i++) {
+                    s3[i] = 0.;
+                }
+            } else if (ndim == 2 && mode == 3) {
+                for (int i=0; i<n_loc[0]*n_loc[1]; i++) {
+                    s2[i] = 0.;
+                }
             }
         }
+
     }
-    
+
 }
 
 friction::~friction() {
     // destructor to deallocate memory
-    
+
     if (no_data) {return;}
-    
+
     delete[] ux;
     delete[] dux;
     delete[] vx;
     delete[] sx;
-    
+
     delete[] u;
     delete[] du;
     delete[] v;
     delete[] s;
     delete[] sn;
-    
+
     for (int i=0; i<nloads; i++) {
         delete loads[i];
     }
-    
+
     delete[] loads;
-    
+
     if (load_file) {
         delete[] s1;
         delete[] s2;
@@ -254,18 +258,18 @@ friction::~friction() {
 
 iffields friction::solve_interface(const boundfields b1, const boundfields b2, const double zp1, const double zs1, const double zp2, const double zs2, const int i, const int j, const double t) {
     // solves boundary conditions for a frictionless interface
-    
+
     ifchar ifcp, ifchatp;
-    
+
     ifcp.v1 = b1.v1;
     ifcp.v2 = b2.v1;
     ifcp.s1 = b1.s11;
     ifcp.s2 = b2.s11;
-    
+
     ifchatp = solve_locked(ifcp,zp1,zp2);
-    
+
     iffields iffin, iffout;
-    
+
     iffin.v12 = b1.v2;
     iffin.v22 = b2.v2;
     iffin.v13 = b1.v3;
@@ -274,30 +278,30 @@ iffields friction::solve_interface(const boundfields b1, const boundfields b2, c
     iffin.s22 = b2.s12;
     iffin.s13 = b1.s13;
     iffin.s23 = b2.s13;
-    
+
     iffout = solve_friction(iffin, ifchatp.s1, zs1, zs2, i, j, t);
-    
+
     iffout.v11 = ifchatp.v1;
     iffout.v21 = ifchatp.v2;
     iffout.s11 = ifchatp.s1;
     iffout.s21 = ifchatp.s2;
-    
+
     return iffout;
-    
+
 }
 
 iffields friction::solve_friction(iffields iffin, double snc, const double z1, const double z2, const int i, const int j, const double t) {
     // solve friction law for shear tractions and slip velocities
-    
+
     const double eta = z1*z2/(z1+z2);
     double phi, phi2, phi3, v2, v3;
-    
+
     // calculate index
-    
+
     int index = i*n_loc[1]+j;
-    
+
     // add boundary loads
-    
+
     if (load_file) {
         snc += s1[index];
         iffin.s12 += s2[index];
@@ -305,7 +309,7 @@ iffields friction::solve_friction(iffields iffin, double snc, const double z1, c
         iffin.s13 += s3[index];
         iffin.s23 += s3[index];
     }
-    
+
     for (int k=0; k<nloads; k++) {
         snc += loads[k]->get_sn(i,j,t);
         iffin.s12 += loads[k]->get_s2(i,j,t);
@@ -313,33 +317,33 @@ iffields friction::solve_friction(iffields iffin, double snc, const double z1, c
         iffin.s13 += loads[k]->get_s3(i,j,t);
         iffin.s23 += loads[k]->get_s3(i,j,t);
     }
-    
+
     phi2 = eta*(iffin.s12/z1-iffin.v12+iffin.s22/z2+iffin.v22);
     phi3 = eta*(iffin.s13/z1-iffin.v13+iffin.s23/z2+iffin.v23);
     phi = sqrt(pow(phi2,2)+pow(phi3,2));
-    
+
     boundchar b = solve_fs(phi, eta, snc, i, j, t);
-    
+
     iffields iffout;
-    
+
     if (b.v == 0.) {
-        
+
         // fault is locked
-        
+
         v2 = 0.;
         v3 = 0.;
-        
+
     } else {
-        
+
         // fault slips
-        
+
         v2 = b.v*phi2/(eta*b.v+b.s);
         v3 = b.v*phi3/(eta*b.v+b.s);
-        
+
     }
-    
+
     // solve for characteristics
-    
+
     iffout.s12 = phi2-eta*v2;
     iffout.s22 = iffout.s12;
     iffout.s13 = phi3-eta*v3;
@@ -348,9 +352,9 @@ iffields friction::solve_friction(iffields iffin, double snc, const double z1, c
     iffout.v22 = (-iffout.s22+iffin.s22)/z2+iffin.v22;
     iffout.v13 = (iffout.s13-iffin.s13)/z1+iffin.v13;
     iffout.v23 = (-iffout.s23+iffin.s23)/z2+iffin.v23;
-    
+
     // set interface variables to hat variables
-    
+
     v[index] = b.v;
     s[index] = b.s;
     sn[index] = snc;
@@ -372,38 +376,38 @@ iffields friction::solve_friction(iffields iffin, double snc, const double z1, c
                     sx[index] = iffout.s13;
             }
     }
-    
+
     // subtract boundary loads before returning field values
-    
+
     if (load_file) {
         iffout.s12 -= s2[index];
         iffout.s22 -= s2[index];
         iffout.s13 -= s3[index];
         iffout.s23 -= s3[index];
     }
-    
+
     for (int k=0; k<nloads; k++) {
         iffout.s12 -= loads[k]->get_s2(i,j,t);
         iffout.s22 -= loads[k]->get_s2(i,j,t);
         iffout.s13 -= loads[k]->get_s3(i,j,t);
         iffout.s23 -= loads[k]->get_s3(i,j,t);
     }
-    
+
     return iffout;
-    
+
 }
 
 boundchar friction::solve_fs(const double phi, const double eta, const double snc, const int i, const int j, const double t) {
     // solves friction law for slip velocity and strength
     // each friction law supplies its own function for calc_mu
-    
+
     boundchar b;
-    
+
     if (snc < 0.) {
         // compressive normal stress
-        
+
         double mu = calc_mu(phi, eta, snc, i, j, t);
-        
+
         if (mu*fabs(snc) > phi) {
             // locked
             b.v = 0.;
@@ -412,109 +416,109 @@ boundchar friction::solve_fs(const double phi, const double eta, const double sn
             // slipping
             b.s = mu*fabs(snc);
             b.v = (phi-b.s)/eta;
-            
+
         }
-        
+
     } else {
         // tensile normal stress, no shear strength
         b.s = 0.;
         b.v = phi/eta;
     }
-    
+
     // if state variable, set time derivative using hat variables
-    
+
     if (has_state) {
         dstatedt[i*n_loc[1]+j] = calc_dstatedt(b.v, b.s, i, j, t);
     }
-    
+
     return b;
 }
 
 double friction::calc_mu(const double phi, const double eta, const double snc, const int i, const int j, const double t) const {
     // calculates friction coefficient
-    
+
     return 0.;
-    
+
 }
 
 void friction::scale_df(const double A) {
     // scale df for state variables by rk constant A
-    
+
     if (no_data) { return; }
-    
+
     for (int i=0; i<(ndim-1)*n_loc[0]*n_loc[1]; i++) {
         dux[i] *= A;
     }
-    
+
     for (int i=0; i<n_loc[0]*n_loc[1]; i++) {
         du[i] *= A;
     }
-    
+
     if (!has_state) { return; }
-    
+
     for (int i=0; i<(ndim-1)*n_loc[0]*n_loc[1]; i++) {
         dstate[i] *= A;
     }
-    
+
 }
 
 void friction::calc_df(const double dt) {
     // calculate df for state variables for rk time step
-    
+
     if (no_data) {return;}
-    
+
     for (int i=0; i<(ndim-1)*n_loc[0]*n_loc[1]; i++) {
         dux[i] += dt*vx[i];
     }
-    
+
     for (int i=0; i<n_loc[0]*n_loc[1]; i++) {
         du[i] += dt*v[i];
     }
 
     if (!has_state) { return; }
-    
+
     for (int i=0; i<n_loc[0]*n_loc[1]; i++) {
         dstate[i] += dt*dstatedt[i];
     }
-    
+
 }
 
 void friction::update(const double B) {
     // updates state variables
-    
+
     if (no_data) {return;}
-    
+
     for (int i=0; i<(ndim-1)*n_loc[0]*n_loc[1]; i++) {
         ux[i] += B*dux[i];
     }
-    
+
     for (int i=0; i<n_loc[0]*n_loc[1]; i++) {
         u[i] += B*du[i];
     }
 
     if (!has_state) { return; }
-    
+
     for (int i=0; i<n_loc[0]*n_loc[1]; i++) {
         state[i] += B*dstate[i];
     }
-    
+
 }
 
 void friction::read_load(const string loadfile, const bool data_proc) {
     // reads load data from input file
-    
+
     // create communicator
-    
+
     MPI_Comm comm;
-    
+
     comm = create_comm(data_proc);
-    
+
     // create MPI subarray for reading distributed array
-    
+
     if (!data_proc) {
-    
+
         int starts[2];
-        
+
         if (direction == 0) {
             starts[0] = xm_loc[1]-xm[1];
             starts[1] = xm_loc[2]-xm[2];
@@ -525,68 +529,68 @@ void friction::read_load(const string loadfile, const bool data_proc) {
             starts[0] = xm_loc[0]-xm[0];
             starts[1] = xm_loc[1]-xm[1];
         }
-        
+
         MPI_Datatype filearray;
-        
+
         MPI_Type_create_subarray(2, n, n_loc, starts, MPI_ORDER_C, MPI_DOUBLE, &filearray);
-        
+
         MPI_Type_commit(&filearray);
-        
+
         // open file
-        
+
         int rc;
         char* filename;
         char filetype[] = "native";
 
         filename = new char [loadfile.size()+1];
         strcpy(filename, loadfile.c_str());
-        
+
         MPI_File infile;
-        
+
         rc = MPI_File_open(comm, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &infile);
-        
+
         delete[] filename;
-        
+
         if(rc != MPI_SUCCESS){
             std::cerr << "Error opening load file in friction.cpp\n";
             MPI_Abort(MPI_COMM_WORLD, rc);
         }
 
         // set view to beginning
-        
+
         MPI_File_set_view(infile, (MPI_Offset)0, MPI_DOUBLE, filearray, filetype, MPI_INFO_NULL);
-        
+
         // read data
-        
+
         MPI_File_read(infile, s1, n_loc[0]*n_loc[1], MPI_DOUBLE, MPI_STATUS_IGNORE);
         MPI_File_read(infile, s2, n_loc[0]*n_loc[1], MPI_DOUBLE, MPI_STATUS_IGNORE);
         MPI_File_read(infile, s3, n_loc[0]*n_loc[1], MPI_DOUBLE, MPI_STATUS_IGNORE);
-        
+
         // close file
-        
+
         MPI_File_close(&infile);
-        
+
         MPI_Type_free(&filearray);
-        
+
     }
-    
+
 }
 
 void friction::read_state(const string statefile, const bool data_proc) {
     // reads state data from input file
-    
+
     // create communicator
-    
+
     MPI_Comm comm;
-    
+
     comm = create_comm(data_proc);
-    
+
     // create MPI subarray for reading distributed array
-    
+
     if (!data_proc) {
-        
+
         int starts[2];
-        
+
         if (direction == 0) {
             starts[0] = xm_loc[1]-xm[1];
             starts[1] = xm_loc[2]-xm[2];
@@ -597,54 +601,54 @@ void friction::read_state(const string statefile, const bool data_proc) {
             starts[0] = xm_loc[0]-xm[0];
             starts[1] = xm_loc[1]-xm[1];
         }
-        
+
         MPI_Datatype filearray;
-        
+
         MPI_Type_create_subarray(2, n, n_loc, starts, MPI_ORDER_C, MPI_DOUBLE, &filearray);
-        
+
         MPI_Type_commit(&filearray);
-        
+
         // open file
-        
+
         int rc;
         char* filename;
         char filetype[] = "native";
-        
+
         filename = new char [statefile.size()+1];
         strcpy(filename, statefile.c_str());
-        
+
         MPI_File infile;
-        
+
         rc = MPI_File_open(comm, filename, MPI_MODE_RDONLY, MPI_INFO_NULL, &infile);
-        
+
         delete[] filename;
-        
+
         if(rc != MPI_SUCCESS){
             std::cerr << "Error opening state file in friction.cpp\n";
             MPI_Abort(MPI_COMM_WORLD, rc);
         }
-        
+
         // set view to beginning
-        
+
         MPI_File_set_view(infile, (MPI_Offset)0, MPI_DOUBLE, filearray, filetype, MPI_INFO_NULL);
-        
+
         // read data
-        
+
         MPI_File_read(infile, state, n_loc[0]*n_loc[1], MPI_DOUBLE, MPI_STATUS_IGNORE);
-        
+
         // close file
-        
+
         MPI_File_close(&infile);
-        
+
         MPI_Type_free(&filearray);
-        
+
     }
-    
+
 }
 
 void friction::read_params(const string filename, const bool data_proc) {
     // reads friction parameters from file
-    
+
 }
 
 double friction::calc_dstatedt(const double vhat, const double shat, const int i, const int j, const double t) const {
